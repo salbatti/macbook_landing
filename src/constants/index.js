c2ac065f7b9af1b499ea3bac5d9b6f0f9690dc1c
Steve George <98937566+salbatti@users.noreply.github.com>
2025-12-27 19:23:07 +0530

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks=  [
+                        {label:'Store'},
+                        {label:'Mac'},
+                        {label:'iPhone'},
+                        {label:'watch'},
+                        {label:'Vision'},
+                        {label:'AirPods'},
+                    ]
